fcef436a761835d80fe747fedc2daf7361f72467
Shintaro Kaku <kaku.shintaro@outlook.com>
2020-06-24 23:26:44 +0900

    Add files via upload

---
+++ MyWingetInstallList.ps1
@@ -0,0 +1,30 @@
+winget install -e 7zip.7zip -h
+winget install -e Adobe.AdobeAcrobatReaderDC -h
+winget install -e AgileBits.1Password -h
+winget install -e Amazon.AWSCLI -h
+winget install -e Amazon.Kindle -h
+winget install -e Canonical.Ubuntu -h -h
+winget install -e Docker.DockerDesktop -h
+winget install -e Git.Git -h
+winget install -e GitHub.GitHubDesktop -h
+winget install -e GitHub.cli -h
+winget install -e Google.Chrome -h
+winget install -e Heroku.HerokuCLI -h
+winget install -e Microsoft.AzureCLI -h
+winget install -e Microsoft.PowerBI -h
+winget install -e Microsoft.SQLServerManagementStudio -h
+winget install -e Microsoft.Teams -h
+winget install -e Microsoft.VisualStudioCode -h
+winget install -e Microsoft.VisualStudio.Community -h
+winget install -e Microsoft.WindowsTerminal -h
+winget install -e NickeManarin.ScreenToGif -h
+winget install -e OBSProject.OBSStudio -h
+winget install -e Oracle.MySQL -h
+winget install -e Postman.Postman -h
+winget install -e Rubyinstall -eerTeam.Ruby -h
+winget install -e SlackTechnologies.Slack -h
+winget install -e TeraTermProject.teraterm -h
+winget install -e VideoLAN.VLC -h
+winget install -e WiresharkFoundation.Wireshark -h
+winget install -e Zoom.Zoom -h
+winget install -e twitch.twitch -h
