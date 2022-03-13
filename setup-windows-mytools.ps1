b381351a5cd9971333dcf55a9ad4e1ba9824f176
thdy <mail@thdy.jp>
2022-03-13 23:06:41 +0900

    add m365 module

---
+++ setup-windows-mytools.ps1
@@ -0,0 +1,19 @@
+winget install -e 7zip.7zip -h
+winget install -e Adobe.AdobeAcrobatReaderDC -h
+winget install -e AgileBits.1Password -h
+winget install -e Amazon.AWSCLI -h
+winget install -e Canonical.Ubuntu -h -h
+winget install -e Git.Git -h
+winget install -e GitHub.cli -h
+winget install -e Google.Chrome -h
+winget install -e Heroku.HerokuCLI -h
+winget install -e Microsoft.AzureCLI -h
+winget install -e Microsoft.VisualStudioCode -h
+winget install -e Microsoft.WindowsTerminal -h
+winget install -e NickeManarin.ScreenToGif -h
+winget install -e SlackTechnologies.Slack -h
+winget install -e Zoom.Zoom -h
+winget install -e twitch.twitch -h
+
+Install-Module -Name MSOnline
+Install-Module -Name AzureAD
