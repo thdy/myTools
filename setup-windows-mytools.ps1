65851f1083b6d74bcc904a0f177a98cd448ce4c4
thdy <mail@thdy.jp>
2022-06-04 23:37:52 +0900

    fix install package

--- setup-windows-mytools.ps1
+++ setup-windows-mytools.ps1
@@ -1,19 +1,21 @@
-winget install -e 7zip.7zip -h
-winget install -e Adobe.AdobeAcrobatReaderDC -h
-winget install -e AgileBits.1Password -h
-winget install -e Amazon.AWSCLI -h
-winget install -e Canonical.Ubuntu -h -h
-winget install -e Git.Git -h
-winget install -e GitHub.cli -h
-winget install -e Google.Chrome -h
-winget install -e Heroku.HerokuCLI -h
-winget install -e Microsoft.AzureCLI -h
-winget install -e Microsoft.VisualStudioCode -h
-winget install -e Microsoft.WindowsTerminal -h
-winget install -e NickeManarin.ScreenToGif -h
-winget install -e SlackTechnologies.Slack -h
-winget install -e Zoom.Zoom -h
-winget install -e twitch.twitch -h
+winget install 7zip.7zip -h
+winget install Adobe.Acrobat.Reader.64-bit -h
+winget install AgileBits.1Password -h
+winget install Amazon.AWSCLI -h
+winget install Canonical.Ubuntu -h -h
+winget install Git.Git -h
+winget install GitHub.cli -h
+winget install Google.Chrome -h
+winget install Heroku.HerokuCLI -h
+winget install Microsoft.AzureCLI -h
+winget install Microsoft.VisualStudioCode -h
+winget install Microsoft.WindowsTerminal -h
+winget install Microsoft.PowerToys -h
+winget install SlackTechnologies.Slack -h
+winget install Zoom.Zoom -h
+winget install twitch.twitch -h
 
 Install-Module -Name MSOnline
 Install-Module -Name AzureAD
+
+wsl --install
